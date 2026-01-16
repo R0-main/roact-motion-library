1ddf907f2383eaa78fde6e9f01a2bd1ab2d12df9
R0-main <justromixx@gmail.com>
2026-01-16 13:38:51 +0100

    Wrap MotionGradient in a uigradient for enhanced color transition effects

--- motion/motion-rgb-gradient.tsx
+++ motion/motion-rgb-gradient.tsx
@@ -11,6 +11,22 @@ export class MotionRgbGradient extends Roact.Component<MotionRgbGradientProps> {
 	render() {
 		const { Duration, RotationSpeed, Looped = true } = this.props;
 
-		return <MotionGradient Duration={Duration} RotationSpeed={RotationSpeed} Looped={Looped} Rotate={true} />;
+		return (
+			<uigradient
+				Color={
+					new ColorSequence([
+						new ColorSequenceKeypoint(0, Color3.fromRGB(255, 0, 0)),
+						new ColorSequenceKeypoint(0.166, Color3.fromRGB(255, 255, 0)),
+						new ColorSequenceKeypoint(0.333, Color3.fromRGB(0, 255, 0)),
+						new ColorSequenceKeypoint(0.5, Color3.fromRGB(0, 255, 255)),
+						new ColorSequenceKeypoint(0.666, Color3.fromRGB(0, 0, 255)),
+						new ColorSequenceKeypoint(0.833, Color3.fromRGB(255, 0, 255)),
+						new ColorSequenceKeypoint(1, Color3.fromRGB(255, 0, 0)),
+					])
+				}
+			>
+				<MotionGradient Duration={Duration} RotationSpeed={RotationSpeed} Looped={Looped} Rotate={true} />
+			</uigradient>
+		);
 	}
 }
